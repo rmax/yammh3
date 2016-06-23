d823bc618ef1487f707ac9b6364996bf6d00320d
Rolando Espinoza <darkrho@gmail.com>
2016-06-23 07:46:21 -0300

    Listed Python 3.4 as category.

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setup(
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
     ext_modules=list(find_extensions('src', '*.pyx', **dict(
